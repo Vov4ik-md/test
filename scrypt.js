11f45c99b99ce44b85d03af5d7d7c5bc7337f728
Vov4ik-md <139490040+Vov4ik-md@users.noreply.github.com>
2023-09-09 21:49:48 +0300

    first commit

---
+++ scrypt.js
@@ -0,0 +1,47 @@
+//Prima varianta
+
+let vadimsWeight = 78
+
+let vadimsHeight = 1.68
+
+let alexWeight = 92
+
+let alexHeight = 1.95
+
+let vadimsBMI = vadimsWeight / vadimsHeight ** 2
+
+let alexBMI = alexWeight / alexHeight ** 2
+
+let markHigherBMI = vadimsBMI > alexBMI
+
+console.log('Prima varianta')
+
+console.log('Vadim are BMI ' + vadimsBMI)
+
+console.log('Alex are BMI ' + alexBMI)
+
+console.log('BMI al lui Vadim este mai mare decat al lui Alex? ' + markHigherBMI)
+
+// A doua varianta
+
+let vadimsWeight1 = 95
+
+let vadimsHeight1 = 1.88
+
+let alexWeight1 = 85
+
+let alexHeight1 = 1.76
+
+let vadimsBMI1 = vadimsWeight1 / vadimsHeight1 ** 2
+
+let alexBMI1 = alexWeight1 / alexHeight1 ** 2
+
+let markHigherBMI1 = vadimsBMI1 > alexBMI1
+
+console.log('A doua varianta')
+
+console.log('Vadim are BMI ' + vadimsBMI1)
+
+console.log('Alex are BMI ' + alexBMI1)
+
+console.log('BMI al lui Vadim este mai mare decat al lui Alex? ' + markHigherBMI1)
