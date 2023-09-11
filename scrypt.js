a4acde05313b05eb23b736d1aba3d205693e8cf8
Vov4ik-md <vlad_borgan@outlook.com>
2023-09-11 19:59:49 +0300

    lectia din 11.09

--- scrypt.js
+++ scrypt.js
@@ -49,7 +49,7 @@
 
 // exercitiu 1
 
-let number1 = 25
+let number1 = 35
 
 let number2 = 35
 
@@ -61,10 +61,10 @@ console.log(`Variabila 1 - ${number1}, variabila 2 - ${number2}`)
 console.log(`Care din cele 2 variabile declarate este mai mare:`)
 
 if (number1 > number2) {
-	console.log(`${number1} este mai mare`)
+	console.log(`Variabila 1 - ${number1} este mai mare`)
 }
 else {
-	console.log(`Variaila 2 - ${number2}, este mai mare`)
+	console.log(`Variabila 2 - ${number2}, este mai mare`)
 }
 
 //exercitiul 2
@@ -172,11 +172,11 @@ let secondNum = -3
 
 console.log(`Al 2-lea numar este : ${secondNum}`)
 
-let thirdNum = 15
+let thirdNum = 258
 
 console.log(`Al 3-lea numar este : ${thirdNum}`)
 
-let fourthNum = 22
+let fourthNum = 258
 
 console.log(`Al 4-lea numar este : ${fourthNum}`)
 
@@ -184,19 +184,110 @@ let fifthNum = 0
 
 console.log(`Al 5-lea numar este : ${fifthNum}`)
 
-if (firstNum > secondNum && firstNum > thirdNum && firstNum > fourthNum && firstNum > fifthNum) {
+if (firstNum >= secondNum && firstNum >= thirdNum && firstNum >= fourthNum && firstNum >= fifthNum) {
 	console.log(`Cel mai mare numar este ${firstNum}`)
 }
-else if (secondNum > thirdNum && secondNum > fourthNum && secondNum > fifthNum) {
+else if (secondNum >= thirdNum && secondNum >= fourthNum && secondNum >= fifthNum) {
 	console.log(`Cel mai mare numar este ${secondNum}`)
 }
-else if (thirdNum > fourthNum && thirdNum > fifthNum) {
+else if (thirdNum >= fourthNum && thirdNum >= fifthNum) {
 	console.log(`Cel mai mare numar este ${thirdNum}`)
 }
-else if (fourthNum > fifthNum) {
+else if (fourthNum >= fifthNum) {
 	console.log(`Cel mai mare numar este ${fourthNum}`)
 }
 else {
 	console.log(`Cel mai mare numar este ${fifthNum}`)
 }
 
+// exercitiu - Equality operator
+
+//const favoriteNumber = Number(prompt('Care este numarul preferat?'))
+
+const favoriteNumber = 56
+
+console.log(favoriteNumber)
+
+console.log(typeof favoriteNumber)
+
+if (favoriteNumber == 23) { //nu avem nevoie de strict equality deoarece favoriteNumber a primit valoarea 'Number' inainte de prompt
+	console.log('23 este numar favorit')
+} else if (favoriteNumber === 56) {
+	console.log(favoriteNumber, 'este numar frumos')
+} else {
+	console.log('nu cunosc asa numar')
+}
+
+if (favoriteNumber != 23) {
+	console.log('de ce nu 23?');
+}
+
+
+// logical operators
+
+const hasDriverLicense = true
+
+const hasGoodVision = true
+
+const isTired = true
+
+//AND operator (&&)
+
+if (hasDriverLicense && hasGoodVision && !isTired) {
+	console.log('Ana can drive')
+} else {
+	console.log('Ana can not drive')
+}
+
+// OR operator (||)
+
+if (!hasDriverLicense || !hasGoodVision || isTired) {
+	console.log('Ana can not drive')
+} else {
+	console.log('Ana can drive')
+}
+
+
+// switch statement
+
+const myDay = "thuesday"
+
+switch (myDay) {
+	case "monday":
+		console.log('Curs FE')
+		break
+	case "thuesday":
+	case "wednesday":
+		console.log('lectii de pian')
+		break
+	case "thursday":
+		console.log('sala de forta')
+		break
+	case "friday":
+		console.log('curs FE')
+		break
+	case "saturday":
+	case "sunday":
+		console.log('weekend!!!')
+		break
+	default:
+		console.log('nu exista asa zi')
+}
+
+//The conditional (Ternary) Operator
+
+// variabile = expression1 ? expression2 : expression3
+
+const yourAge = 16
+
+yourAge >= 18 ? console.log('drink wine') : console.log('drink water')
+
+let drink2
+
+if (yourAge >= 18) {
+	drink2 = 'wine'
+} else {
+	drink2 = 'water'
+}
+
+console.log(`You can ${yourAge >= 18 ? 'drink wine' : 'drink water'}`);
